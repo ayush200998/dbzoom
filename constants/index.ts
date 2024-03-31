c26228c356dd5664c45b3f351fe3c90b6741ca28
Ayush Khakharia <ayush@herm.is>
2024-04-01 01:20:38 +0530

    Project setup.

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const sidebarLinks = [
+    {
+        label: 'Home',
+        route: '/',
+        icon: '/icons/Home.svg',
+    },
+    {
+        label: 'Upcoming',
+        route: '/upcoming',
+        icon: '/icons/upcoming.svg',
+    },
+    {
+        label: 'Previous Meetings',
+        route: '/previous',
+        icon: '/icons/previous.svg',
+    },
+    {
+        label: 'Recordings',
+        route: '/recordings',
+        icon: '/icons/Video.svg',
+    },
+    {
+        label: 'Personal Room',
+        route: '/personal-room',
+        icon: '/icons/personal.svg',
+    }
+];
---
+++ src/app/(root)/meeting/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = ({ params }: { params: { id: string }}) => {
+  return (
+    <div>Meeting Room #{params.id}</div>
+  )
+}
+
+export default page
